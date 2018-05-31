e702365f0b6f8e642a52d9f3886e5b58ce1199fb
Andreas <andreas848@hotmail.com>
2018-05-31 15:18:10 +0200

    new droplet
    Conflicts:
    	src/main/java/FunctionLayer/BOMCalculator.java

--- src/main/java/DBAccess/Connector.java
+++ src/main/java/DBAccess/Connector.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class Connector
 {
 
-    private static final String URL = "jdbc:mysql://159.89.109.181:3306/fogdb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://206.81.24.186:3306/fogdb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 //    private static final String URL = "jdbc:mysql://localhost:3306/fogdb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 //    private static final String URL = "jdbc:mysql://46.101.253.149:3306/fogdb";
     private static final String USERNAME = "doorkeeper";
